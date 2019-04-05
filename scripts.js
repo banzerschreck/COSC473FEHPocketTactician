284c2e92486ac15901e4ffa61738c5d2288228fe
Ethan Turner <banzerschreck@gmail.com>
2019-04-05 19:44:34 -0400

    Refactored skills.html javascript
    JS for skill.html no longer injects html into the page
    Actually it kind of does but idk man
    Before, I was using element.innerHTML = "some long string with html elements"
    Now I'm using document.createElement("td") for example and appending that with parentElement.appendChild(newTDElement)

--- scripts.js
+++ scripts.js
@@ -9,16 +9,6 @@ var heroData = new Object();
  */
 function displayHeroesList() {
   console.log("Building Heroes list...");
-  /*var s = "<ul>";
-  for (var i=0; i<heroData.length; i++) {
-    s+="<li><a href=\"javascript:displayHeroData("+i+")\">";
-    s += heroData[i].name + ": " + heroData[i].title + "</a></li>";
-    console.log("Added hero #" + i + ": " + heroData[i].name);
-  }
-  s+="</ul>";
-  
-  console.log("Built Heroes list! Displaying");  
-  document.getElementById("select").innerHTML = s;*/
   const heroesList = document.getElementById("heroesList");
   for(var i = 0; i < heroData.length; i++) {
     var li = document.createElement("li");
@@ -102,46 +92,6 @@ function displayHeroData(heroid) {
     tr.appendChild(tdSType);
     heroSkillsTable.appendChild(tr);
   }  
-  /*s += "<img src=\"data/" + hero.assets.main + "\"/><br />";
-  //name and title
-  s+= "<h2>"+hero.name+": "+hero.title+"</h2><br />";
-  //weapon, color, and movement
-  s+= "<img src=\'data/assets/skills/"+hero.color+hero.weapon+".png\'><img src=\'data/assets/skills/"+hero.movetype+".png\'><br />";
-  //stats
-  s+= "<h3>Stats</h3>";
-  s += "<table><tr><th>Rarity/Lvl</th><th>HP</th><th>Atk</th><th>Spd</th><th>Def</th><th>Res</th></tr>";
-  //console.log(hero.stats);
-  if (hero.hasIV) {
-    for (const o of hero.stats) {
-      s+="<tr><td>"+o.rank+"</td>";
-      s += "<td>" + o.hp[0].$numberInt + ", " + o.hp[1].$numberInt + ", " + o.hp[2].$numberInt+"</td>";
-      s += "<td>" + o.atk[0].$numberInt + ", " + o.atk[1].$numberInt + ", " + o.atk[2].$numberInt+"</td>";
-      s += "<td>" + o.spd[0].$numberInt + ", " + o.spd[1].$numberInt + ", " + o.spd[2].$numberInt+"</td>";
-      s += "<td>" + o.def[0].$numberInt + ", " + o.def[1].$numberInt + ", " + o.def[2].$numberInt+"</td>";
-      s += "<td>" + o.res[0].$numberInt + ", " + o.res[1].$numberInt + ", " + o.res[2].$numberInt+"</td></tr>";
-    }
-  } else {
-    for (const o of hero.stats) {
-      s += "<tr><td>" + o.rank+"</td>";
-      s += "<td>" + o.hp.$numberInt+"</td>";
-      s += "<td>" + o.atk.$numberInt+"</td>";
-      s += "<td>" + o.spd.$numberInt+"</td>";
-      s += "<td>" + o.def.$numberInt+"</td>";
-      s += "<td>" + o.res.$numberInt+"</td></tr>";
-    }
-  }
-  s+= "</table>";
-  //skills
-  s += "<h3>Skills</h3>";
-  s += "<table><tr><th>Skill Name</th><th>Rarity</th><th>Skill Type</th></tr>";
-  console.log(hero.skills);
-  for (const o of hero.skills) {
-    s += "<tr><td>"+o.name+"</td>";
-    s += "<td><img src=\"data/assets/ui/stars-" + o.rarity+"star.png\"></img></td>";
-    s += "<td><img src=\"data/assets/skills/" + o.type+".png\"></td></tr>";
-  }
-  s+="</table>";
-  document.getElementById("hero").innerHTML = s;*/
 }
 
 //------------------------------Skills-----------------------------------------------
@@ -151,28 +101,211 @@ var skillsData = new Object();
  */
 function displaySkillsList() {
   console.log("Building Skills list...");
-
-  var s = "<ul>";
-  for (var i=0; i<skillsData.length; i++) {
-    s+="<li><a href=\"javascript:displaySkillData("+i+")\">";
-    s+= skillsData[i].name;
-    s+="</a></li>";
+  const skillsList = document.getElementById("skillsList");
+  for(var i = 0; i < skillsData.length; i++) {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = "javascript:displaySkillData(" + i + ")";
+    a.innerHTML = skillsData[i].name;
+    li.appendChild(a);
+    skillsList.appendChild(li);
   }
-  s+="</ul>";
-  document.getElementById("select").innerHTML = s;
 }
 /*
  * displays data for a given skill in a container called "skill"
  */
 function displaySkillData(skillId) {
-  var s = "";
-  var p = "";
+  document.getElementById("skill").className = "shown";
+  //var s = "";
+  //var p = "";
   var skill = skillsData[skillId];
 
   console.log("Displaying skill data for skill #", skillId);
   console.log(skillsData[skillId]);
-
-  s += "<h3>" + skill.name + "</h3>";
+  //name
+  const skillName = document.getElementById("skillName");
+  skillName.innerHTML = skill.name;
+  //class/type image
+  const skillClass = document.getElementById("skillClass");
+  if(skill.class === 'weapon') {
+    skillClass.src = "data/assets/skills/" + skill.color + skill.type + ".png";
+  } else {
+    skillClass.src = "data/assets/skills/" + skill.class + ".png";
+  }
+  //effect
+  const skillEffect = document.getElementById("skillEffect");
+  skill.effect ? skillEffect.innerHTML = skill.effect : skillEffect.innerHTML = "";
+  //sp
+  const skillSP = document.getElementById("skillSP");
+  skillSP.innerHTML = skill.sp.$numberInt;
+  //inheritable
+  const skillInheritable = document.getElementById("skillInheritable");
+  skill.inheritable ? skillInheritable.innerHTML = "Yes" : skillInheritable.innerHTML = "No";
+  
+  const skillIsWeapon = document.getElementById("skillIsWeapon");
+  const skillIsSpecial = document.getElementById("skillIsSpecial");
+  const skillIsAssist = document.getElementById("skillIsAssist");
+  skillIsWeapon.className = "hidden";
+  skillIsSpecial.className = "hidden";
+  skillIsAssist.className = "hidden";
+  //is weapon
+  if(skill.class === "weapon") {
+    skillIsWeapon.className = "shown";
+    //stats
+    const skillStats = document.getElementById("skillStats");
+    var s = "HP: " + skill.stats[0].$numberInt + ", ";
+    s += "Atk: " + skill.stats[1].$numberInt + ", ";
+    s += "Spd: " + skill.stats[2].$numberInt + ", ";
+    s += "Def: " + skill.stats[3].$numberInt + ", ";
+    s += "Res: " + skill.stats[4].$numberInt;
+    skillStats.innerHTML = s;
+    const skillHasRefines = document.getElementById("skillHasRefines");
+    if(skill.refines) {//refineable
+      skillHasRefines.className = "shown";
+      //refine type
+      const skillRefineType = document.getElementById("skillRefineType");
+      skillRefineType.innerHTML = skill.refines.type;
+      //refine cost
+      const skillRefineCost = document.getElementById("skillRefineCost");
+      skillRefineCost.innerHTML = skill.refines.cost.$numberInt;
+      //refines table
+      const skillRefinesTable = document.getElementById("skillRefinesTable");
+      skillRefinesTable.innerHTML = "";
+      if(skill.refines.neweffect) {
+        const srEffRow = document.createElement("tr");
+        const srEffRefine = document.createElement("td");
+        const srEffStats = document.createElement("td");
+        const srEffEffect = document.createElement("td");
+        srEffRefine.innerHTML = "Effect";
+        s = "HP: " + skill.refines.eff.stats[0].$numberInt + ", ";
+        s += "Atk: " + skill.refines.eff.stats[1].$numberInt + ", ";
+        s += "Spd: " + skill.refines.eff.stats[2].$numberInt + ", ";
+        s += "Def: " + skill.refines.eff.stats[3].$numberInt + ", ";
+        s += "Res: " + skill.refines.eff.stats[4].$numberInt;
+        srEffStats.innerHTML = s;
+        const srEffNewEffectSpan = document.createElement("span");
+        srEffNewEffectSpan.innerHTML = skill.refines.eff.effect;
+        srEffNewEffectSpan.id = "skillRefinesTableNewEffectSpan";
+        const srEffNewEffectBR = document.createElement("br");
+        if(skill.refines.neweffect) {
+          srEffEffect.innerHTML = skill.refines.neweffect;
+        } else if(skill.effect) {
+          srEffEffect.innerHTML = skill.effect;
+        } else {
+          srEffEffect.innerHTML = "";
+        }
+        srEffEffect.appendChild(srEffNewEffectBR);
+        srEffEffect.appendChild(srEffNewEffectSpan);
+        srEffRow.appendChild(srEffRefine);
+        srEffRow.appendChild(srEffStats);
+        srEffRow.appendChild(srEffEffect);
+        skillRefinesTable.appendChild(srEffRow);
+      }
+      //atk refine
+      const srAtkRow = document.createElement("tr");
+      const srAtkRefine = document.createElement("td");
+      const srAtkStats = document.createElement("td");
+      const srAtkEffect = document.createElement("td");
+      srAtkRefine.innerHTML = "Attack";
+      s = "HP: " + skill.refines.atk.stats[0].$numberInt + ", ";
+      s += "Atk: " + skill.refines.atk.stats[1].$numberInt + ", ";
+      s += "Spd: " + skill.refines.atk.stats[2].$numberInt + ", ";
+      s += "Def: " + skill.refines.atk.stats[3].$numberInt + ", ";
+      s += "Res: " + skill.refines.atk.stats[4].$numberInt;
+      srAtkStats.innerHTML = s;
+      if(skill.refines.neweffect) {
+        srAtkEffect.innerHTML = skill.refines.neweffect
+      } else if(skill.effect) {
+        srAtkEffect.innerHTML = skill.effect;
+      } else {
+        srAtkEffect.innerHTML = "";
+      }
+      srAtkRow.appendChild(srAtkRefine);
+      srAtkRow.appendChild(srAtkStats);
+      srAtkRow.appendChild(srAtkEffect);
+      skillRefinesTable.appendChild(srAtkRow);
+      //spd refine
+      const srSpdRow = document.createElement("tr");
+      const srSpdRefine = document.createElement("td");
+      const srSpdStats = document.createElement("td");
+      const srSpdEffect = document.createElement("td");
+      srSpdRefine.innerHTML = "Speed";
+      s = "HP: " + skill.refines.spd.stats[0].$numberInt + ", ";
+      s += "Atk: " + skill.refines.spd.stats[1].$numberInt + ", ";
+      s += "Spd: " + skill.refines.spd.stats[2].$numberInt + ", ";
+      s += "Def: " + skill.refines.spd.stats[3].$numberInt + ", ";
+      s += "Res: " + skill.refines.spd.stats[4].$numberInt;
+      srSpdStats.innerHTML = s;
+      if(skill.refines.neweffect) {
+        srSpdEffect.innerHTML = skill.refines.neweffect
+      } else if(skill.effect) {
+        srSpdEffect.innerHTML = skill.effect;
+      } else {
+        srSpdEffect.innerHTML = "";
+      }
+      srSpdRow.appendChild(srSpdRefine);
+      srSpdRow.appendChild(srSpdStats);
+      srSpdRow.appendChild(srSpdEffect);
+      skillRefinesTable.appendChild(srSpdRow);
+      //def refine
+      const srDefRow = document.createElement("tr");
+      const srDefRefine = document.createElement("td");
+      const srDefStats = document.createElement("td");
+      const srDefEffect = document.createElement("td");
+      srDefRefine.innerHTML = "Defense";
+      s = "HP: " + skill.refines.def.stats[0].$numberInt + ", ";
+      s += "Atk: " + skill.refines.def.stats[1].$numberInt + ", ";
+      s += "Spd: " + skill.refines.def.stats[2].$numberInt + ", ";
+      s += "Def: " + skill.refines.def.stats[3].$numberInt + ", ";
+      s += "Res: " + skill.refines.def.stats[4].$numberInt;
+      srDefStats.innerHTML = s;
+      if(skill.refines.neweffect) {
+        srDefEffect.innerHTML = skill.refines.neweffect
+      } else if(skill.effect) {
+        srDefEffect.innerHTML = skill.effect;
+      } else {
+        srDefEffect.innerHTML = "";
+      }
+      srDefRow.appendChild(srDefRefine);
+      srDefRow.appendChild(srDefStats);
+      srDefRow.appendChild(srDefEffect);
+      skillRefinesTable.appendChild(srDefRow);
+      //res refine
+      const srResRow = document.createElement("tr");
+      const srResRefine = document.createElement("td");
+      const srResStats = document.createElement("td");
+      const srResEffect = document.createElement("td");
+      srResRefine.innerHTML = "Resistance";
+      s = "HP: " + skill.refines.res.stats[0].$numberInt + ", ";
+      s += "Atk: " + skill.refines.res.stats[1].$numberInt + ", ";
+      s += "Spd: " + skill.refines.res.stats[2].$numberInt + ", ";
+      s += "Def: " + skill.refines.res.stats[3].$numberInt + ", ";
+      s += "Res: " + skill.refines.res.stats[4].$numberInt;
+      srResStats.innerHTML = s;
+      if(skill.refines.neweffect) {
+        srResEffect.innerHTML = skill.refines.neweffect
+      } else if(skill.effect) {
+        srResEffect.innerHTML = skill.effect;
+      } else {
+        srResEffect.innerHTML = "";
+      }
+      srResRow.appendChild(srResRefine);
+      srResRow.appendChild(srResStats);
+      srResRow.appendChild(srResEffect);
+      skillRefinesTable.appendChild(srResRow);
+    } else {
+      skillHasRefines.className = "hidden";
+    }
+  } else if(skill.class === "special") {//is special
+    skillIsSpecial.className = "shown";
+    const skillCooldown = document.getElementById("skillCooldown");
+    skillCooldown.innerHTML = skill.cooldown.$numberInt;
+  } else if(skill.class === "assist") {//is assist
+    skillIsAssist.className = "shown";
+    const skillRange = document.getElementById("skillRange");
+    skillRange.innerHTML = skill.range.$numberInt;
+  }
+  /*s += "<h3>" + skill.name + "</h3>";
   if(skill.class=="weapon") {
     s += "<img src=\"data/assets/skills/" + skill.color + skill.type + ".png\"><br />";
   } else {
@@ -267,9 +400,9 @@ function displaySkillData(skillId) {
   } else if(skill.class=="assist") {
     s += "Range: " + skill.range.$numberInt + "<br />";
   }
-  document.getElementById("skill").innerHTML = s;
+  document.getElementById("skill").innerHTML = s;*/
 }
-//======================STITCH USER AUTH/REGISTRATION=========================================
+/*======================STITCH USER AUTH/REGISTRATION=========================================*/
 const {
   Stitch,
   UserPasswordAuthProviderClient
